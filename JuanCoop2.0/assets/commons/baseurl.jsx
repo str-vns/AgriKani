9b9269a8b862cd7b0780f127ad42fc26ec759461
Flappy <stvnsbarrantes@gmail.com>
2024-12-03 22:13:31 +0800

    assets

--- JuanCoop2.0/assets/commons/baseurl.jsx
+++ JuanCoop2.0/assets/commons/baseurl.jsx
@@ -4,8 +4,10 @@ console.log(config.BASE_ADDRESS, "config");
 let baseURL = '';
 
 {Platform.OS === 'android' || Platform.OS === 'ios'
-? baseURL = `https://agrikani.onrender.com/api/v2/`
+? baseURL = `http://192.168.50.236:4000/api/v2/`
 : baseURL = `${config.BASE_ADDRESS_1}`
 }
 
 export default baseURL;
+
+// https://agrikani.onrender.com/api/v2/
