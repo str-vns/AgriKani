4bdd14264fe981ae7b9d1b33c04641f1a10777a1
Flappy <stvnsbarrantes@gmail.com>
2024-12-19 19:48:43 +0800

    routes up

---
+++ AgriKaniAPI/routes/member.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const memberControllers = require("../controllers/memberControllers");
+const { verifyJWT, authorizeRoles } = require("../middleware/verifyJWT");
+const { METHOD, PATH, ROLE } = require("../constants/index");
+const { path } = require("pdfkit");
+
+
+const memberRoutes = [
+    {
+        method: METHOD.POST,
+        path: PATH.MEMBER,
+        roles: [],
+        handler: memberControllers.CreateMember,
+    },
+    {
+        method: METHOD.GET,
+        path: PATH.MEMBER,
+        roles: [],
+        handler: memberControllers.GetMember,
+    },
+    {
+        method: METHOD.PUT,
+        path: PATH.MEMBER_ID,
+        roles: [],
+        handler: memberControllers.UpdateMember,
+    },
+    {
+        method: METHOD.DELETE,
+        path: PATH.MEMBER_ID,
+        roles: [],
+        handler: memberControllers.DeleteMember,
+    },
+    {
+        method: METHOD.GET,
+        path: PATH.MEMBER_ID,
+        roles: [],
+        handler: memberControllers.SingleMember,
+    },
+    {
+        method: METHOD.PATCH,
+        path: PATH.MEMBER_APPROVED_ID,
+        roles: [],
+        handler: memberControllers.ApprovedMember,
+    },
+    {
+        method: METHOD.PATCH,
+        path: PATH.MEMBER_DISAPPROVED_ID,
+        roles: [],
+        handler: memberControllers.DisapprovedMember,
+    }
+];
+
+memberRoutes.forEach((route) => {
+    const { method, path, roles = [], handler } = route;
+    router[method](path, authorizeRoles(...roles), handler);
+});
