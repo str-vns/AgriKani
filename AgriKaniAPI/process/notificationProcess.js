d74a66f154858da8b1fc4161e3f26be04e550f27
Flappy <stvnsbarrantes@gmail.com>
2025-01-14 06:14:36 +0800

    update

--- AgriKaniAPI/process/notificationProcess.js
+++ AgriKaniAPI/process/notificationProcess.js
@@ -8,7 +8,19 @@ const serviceAccount = require('../notifacation-ko-firebase-adminsdk-sahjo-8e50c
 
 // NOTE Three DOTS MEANS OK IN COMMENT
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    projectId: process.env.PROJECT_ID,
+    privateKeyId: process.env.PRIVATE_KEY_ID,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.CLIENT_EMAIL,
+    clientId: process.env.CLIENT_ID,
+    authUri: process.env.AUTH_URI,
+    tokenUri: process.env.TOKEN_URI,
+    authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
+    universeDomain: process.env.UNIVERSE_DOMAIN  
+  })
 })
 
 //create ...
