ad70481a4f06700dcfc752359cf55de8cec42e18
Flappy <stvnsbarrantes@gmail.com>
2025-03-08 21:44:43 +0800

    up

--- AgriKaniAPI/app.js
+++ AgriKaniAPI/app.js
@@ -39,15 +39,26 @@ app.use(cookieParser());
 
 app.get("/payment-redirect", (req, res) => {
   const paymentIntentId = req.query.payment_intent_id;
-  const appDeepLink = `juanCoop://payment-success`;
-  // const fallbackUrl = `https://your-website.com/payment-success?payment_intent_id=${paymentIntentId}`;
+  const appDeepLink = `juanCoop://payment-success?payment_intent_id=${paymentIntentId}`;
+  const fallbackUrl = `https://your-website.com/payment-success?payment_intent_id=${paymentIntentId}`;
+
   res.send(`
     <html>
       <head>
-        <meta http-equiv="refresh" content="0;url=${appDeepLink}" />
+        <title>Redirecting...</title>
+        <script type="text/javascript">
+          setTimeout(function() {
+            window.location.replace("${appDeepLink}");
+          }, 100); // Less aggressive than location.href
+
+          setTimeout(function() {
+            window.location.replace("${fallbackUrl}");
+          }, 3000); // If app is not installed, fallback after 3s
+        </script>
       </head>
       <body>
-        <p>Redirecting to app... If nothing happens, <a href="${appDeepLink}">click here</a>.</p>
+        <p>Redirecting to app...</p>
+        <p>If nothing happens, <a href="${appDeepLink}">click here</a>.</p>
       </body>
     </html>
   `);
